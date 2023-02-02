ecc3e1230955a4c3dcfa515cbad1d39bc8c22396
iampowell <powellhabwe@gmail.com>
2023-02-02 15:48:56 +0300

    commit

--- Django_Project/settings.py
+++ Django_Project/settings.py
@@ -178,4 +178,4 @@ AWS_STORAGE_BUCKET_NAME = "django-eventmanagementsystem"
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
